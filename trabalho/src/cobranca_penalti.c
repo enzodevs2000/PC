6c300111f4d0d4d2bf528b9022c94235b44638f4
enzodevs2000 <enzodevs2000@gmail.com>
2022-08-30 17:58:03 -0300

    create threads

--- trabalho/src/cobranca_penalti.c
+++ trabalho/src/cobranca_penalti.c
@@ -12,7 +12,10 @@ typedef struct {
     int forca_defesa;
 } Goleiro;
 
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 void * f_batedor(void * arg);
+void * f_goleiro(void *arg);
 
 void main(argc, argv) 
 int argc;
@@ -29,21 +32,48 @@ char* argv[];
         id = (int *) malloc(sizeof(int));
         *id = i;
 	    erro = pthread_create(&(batedores[i]), NULL, f_batedor, (void *) (id));
-        
         if(erro) {
             printf("A thread %d nao foi criada!!!\n", *(int *) id);
         }
         else{
             printf("O batedor %d foi criado!\n", *(int *) id);
+            sleep(2);    
         }
         
     }
 
-    pthread_join(batedores[0], NULL);
+    for (i = 0; i < GOLEIROS; i++) {
+        id = (int *) malloc(sizeof(int));
+        *id = i;
+        erro = pthread_create(&(goleiros[i].goleiro), NULL, f_goleiro, (void *) (id));
+        if(erro) {
+            printf("A thread %d nao foi criada!!!\n", *(int *) id);
+        }
+        else{
+            printf("O goleiro %d foi criado!\n", *(int *) id);
+            sleep(2);    
+        }
+    }
+
+    for (i = 0; i < BATEDORES; i++) {
+        pthread_join(batedores[i], NULL);
+    }
+
+    for (i = 0; i < GOLEIROS; i++) {
+        pthread_join(goleiros[i].goleiro, NULL);
+    }
     
 }
 
 void* f_batedor(void *arg) {
-    //printf("Jogador %d: Bateu!\n", *(int *) arg);
+    //pthread_mutex_lock(&lock);
+    printf("Jogador %d: Bateu!\n", *(int *) arg);
+    
+    //pthread_mutex_unlock(&lock);
+
     pthread_exit(0);
+}
+
+void* f_goleiro(void *arg) {
+    printf("Goleiro %d defendeu!\n", *(int *) arg);
 }
