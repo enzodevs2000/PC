64e4b07e750e29c10dc7c3920849736606b6ff66
enzodevs2000 <enzodevs2000@gmail.com>
2023-01-22 19:55:36 -0300

    TERMINEI

--- trabalho/src/manobrista.c
+++ trabalho/src/manobrista.c
@@ -4,76 +4,74 @@
 #include "unistd.h"
 #include "semaphore.h"
 
-#define N_CARROS 20
-#define FILA_ESTACIONAMENTO 5
+#define N_CARROS 20 // Número de threads representando os carros
+#define FILA_ESTACIONAMENTO 2 // Número de vagas na fila para o estacionamento
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t manobrista_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Lock para garantir exclusão mútua nas variáveis compartilhadas
+pthread_cond_t manobrista_cond = PTHREAD_COND_INITIALIZER; // Variável de condição para o manobrista
 
-sem_t sem_vagas;
-sem_t sem_fila_estacionamento;
+sem_t sem_vagas; // Semáforo que representa a quantidade de vagas do estacionamento
+sem_t sem_fila_estacionamento; // Semáforo que representa a quantidade de vagas que há na fila para o estacionamento
 
-int qtd_carros = 0;
-int carros_fila = 0;
-int qtd_manobristas;
-int qtd_vagas;
+int qtd_carros = 0; // Quantidade de carros que estão estacionados no estacionamento
+int carros_fila = 0; // Quantidade de carros que estão na fila para estacionar
+int carros_embora = 0; // Quantidade de carros que já saíram do estacionamento
 
+int qtd_manobristas; // Quantidade de manobristas que vão trabalhar no estacionamento
+int qtd_vagas; // Quantidade de vagas do estacionamento
+
+void *manobrista(void *arg);
 /*
     Essa função é responsável por simular o comportamento de um manobrista. Ele é uma thread que fica esperando
     um carro chegar, para então estacionar o carro. Se não tiver vagas no estacionamento, ele fica esperando até
     que um carro saia, liberando uma vaga. Após estacionar um carro, ele volta para a fila de carros para estacionar
     o próximo carro.
 */
-void* manobrista(void* arg);
 
+void *carro(void *arg);
 /*
     Essa função é responsável por simular o comportamento de um carro. Ele é uma thread que fica esperando
     um manobrista estacionar o carro. Após estacionar o carro, o motorista irá fazer alguma coisa, o que, demorará
     um certo tempo. Após isso, ele sai do estacionamento liberando uma vaga e a thread é finalizada.
 */
-void* carro(void* arg);
 
-void main(argc, argv)
-int argc;
+void main(argc, argv) int argc;
 char *argv[];
 {
     int i;
     int *id;
+    int vagas;
 
     qtd_manobristas = atoi(argv[1]);
-    qtd_vagas = atoi(argv[2]);
+    vagas = atoi(argv[2]);
+    qtd_vagas = vagas;
 
     pthread_t manobristas[qtd_manobristas];
     pthread_t carros[N_CARROS];
 
     sem_init(&sem_vagas, 0, qtd_vagas);
     sem_init(&sem_fila_estacionamento, 0, FILA_ESTACIONAMENTO);
-    
 
-    for (i = 0; i < qtd_manobristas; i++)
-    {
+    for (i = 0; i < qtd_manobristas; i++) {
         id = (int *)malloc(sizeof(int));
         *id = i;
         pthread_create(&(manobristas[i]), NULL, manobrista, (void *)(id));
     }
 
     for (i = 0; i < N_CARROS; i++) {
-        int* id = (int *)malloc(sizeof(int));
+        int *id = (int *)malloc(sizeof(int));
         *id = i;
         pthread_create(&(carros[i]), NULL, carro, (void *)(id));
     }
 
-    // chega_carros();
-
     for (i = 0; i < N_CARROS; i++) {
-        pthread_join(carros[i], NULL);
+        pthread_join(carros[i], NULL); // O programa é finalizado quando todos os carros já passaram pelo estacionamento,
+                                       //   seja por estacionar ou por não conseguir estacionar
     }
 }
 
-void* manobrista(void* arg)
-{
+void *manobrista(void *arg) {
     int id = *((int *)arg);
-    // printf("Manobrista %d criado\n", id);
     while (1) {
         // Se não tiver carros ou vagas, o manobrista fica esperando
 
@@ -86,52 +84,68 @@ void* manobrista(void* arg)
         pthread_mutex_unlock(&mutex);
 
         // Manobrista estaciona o carro
-
+        sleep(2);
         printf("Manobrista %d estacionou um carro\n\n", id);
 
-        // Quantidade de vagas disponiveis diminui
-        // sem_wait(&sem_vagas);
+        // Quantidade de vagas disponiveis diminui e quantidade de carros no estacionamento aumenta
 
         pthread_mutex_lock(&mutex);
-        // qtd_manobristas--;
+
         qtd_carros++;
         qtd_vagas--;
-        // qtd_manobristas++;
-        // pthread_cond_signal(&carro_cond);
+        carros_fila--; // Sincronização para diminuir a quantidade de carros na fila
+
         pthread_mutex_unlock(&mutex);
     }
 }
 
-
-void* carro(void* arg) {
+void *carro(void *arg) {
     int id = *((int *)arg);
-    // printf("Carro %d chegou\n", id);
-        // Tenta entrar na fila de carros
-        if (sem_trywait(&sem_fila_estacionamento) == 0) {
-            printf("Carro %d entrou na fila\n\n", id);
-            // Da carro ao manobrista
-            printf("Motorista do carro %d dando carro ao manobrista\n\n", id);
-            sem_post(&sem_fila_estacionamento);
+    // Tenta entrar na fila de carros, se conseguir, então diminui a quantidade de permissões do semáforo
+    if (sem_trywait(&sem_fila_estacionamento) == 0) {
+        printf("Carro %d entrou na fila\n\n", id);
+
+        pthread_mutex_lock(&mutex);
+        carros_fila++; // Sincronização para aumentar a quantidade de carros na fila
+        pthread_mutex_unlock(&mutex);
+
+        // Da carro ao manobrista e libera uma vaga na fila dando um UP no semáforo
+        sem_post(&sem_fila_estacionamento);
+
+        printf("Motorista do carro %d dando carro ao manobrista\n\n", id);
+        
+
+        // Se tiver pelo menos um carro na fila, então o manobrista é acordado
+        pthread_mutex_lock(&mutex);
+        if (carros_fila == 1) {
             pthread_cond_signal(&manobrista_cond);
-
-            // Ocupa uma vaga no estacionamento
-            sem_wait(&sem_vagas);
-            // Faz suas tarefas
-            printf("Motorista do carro %d fazendo alguma coisa\n\n", id);
-            sleep(rand() % 20);
-            // Vai embora
-            printf("Carro %d saindo\n\n", id);
-            sem_post(&sem_vagas);
-
         }
-        else {
-            printf("Carro %d não entrou na fila e foi embora\n\n", id);
-        }
-    
+
+        pthread_mutex_unlock(&mutex);
+
+        // Ocupa uma vaga no estacionamento dando um down no semáforo
+        
+        sem_wait(&sem_vagas);
+
+        // Motorista faz suas tarefas
+        printf("Motorista do carro %d fazendo alguma coisa\n\n", id);
+        sleep(5);
+
+        // Motorista vai embora liberando uma vaga no estacionamento e dando um UP no semáforo
+        printf("Carro %d saindo\n\n", id);
+
+        pthread_mutex_lock(&mutex);
+        qtd_vagas++;
+        pthread_mutex_unlock(&mutex);
+
+        sem_post(&sem_vagas);
+    }
+    else { // Se não conseguir entrar na fila, então o carro vai embora
+        // Sincronização para contar o número de carros que foram embora
+        pthread_mutex_lock(&mutex);
+        printf("Carro %d não entrou na fila e foi embora\n\n", id);
+        carros_embora++;
+        printf("Carros que foram embora: %d\n\n", carros_embora);
+        pthread_mutex_unlock(&mutex);
+    }
 }
-
-/*
-    TODO: 
-        Verificar se o programa esta funcionando corretamente
-
-*/
